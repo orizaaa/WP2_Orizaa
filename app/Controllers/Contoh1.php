f7750b457db3381c159ae48b2309187ef42cff0e
unknown <orizaaa.orizap20@gmail.com>
2024-03-26 12:44:29 +0700

    makan siang minum es

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+use App\Controllers\BaseController;
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya Oriza Emerson P
+        Saya tinggal di daerah Bekasi";
+    }
+}
